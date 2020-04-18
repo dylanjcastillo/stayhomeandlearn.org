2952bda01be2aeb0e725aa021af6a936771e3875
dylanjcastillo <dylanjcastillo@users.noreply.github.com>
2020-04-18 18:28:46 +0200

    Added alert for mail subscription

--- site_builder/utils.py
+++ site_builder/utils.py
@@ -6,13 +6,13 @@ SITE_DIR = SRC_DIR / "site"
 DATA_DIR = SRC_DIR / "data"
 
 IGNORED_FILES = ["template.html", ".DS_Store"]
-INVALIDATIONS = ["/index.html"]
+INVALIDATIONS = ["/index.html", "/css/style.css"]
 
 LISTS_MAPPING = {
     "learning_resources": "&#128218; Learning Resources",
     "productivity": "&#128187; Productivity",
     "health": "&#x1F3CB; Health & Fitness",
-    "entertainment": "&#x1F4FA; Entertainment",
+    "entertainment": "&#x1f3ae; Entertainment",
 }
 
 CONTENT_TYPE_MAPPING = {
